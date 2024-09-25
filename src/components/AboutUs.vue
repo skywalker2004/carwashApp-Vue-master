6e5108e92226e4b7ae8bac5fbf7b7c4047e89595
Your Name <kelvinkiprono659@gmail.com>
2024-09-25 16:21:39 +0300

    Add about text data in the script section for About Us component.

--- src/components/AboutUs.vue
+++ src/components/AboutUs.vue
@@ -4,3 +4,12 @@
     <p>{{ about }}</p>
   </div>
 </template>
+<script>
+export default {
+  data() {
+    return {
+      about: "At Bonke Car Wash, we strive to provide the best car care in town."
+    };
+  }
+};
+</script>
